6ec07d4dbd7c89ee9a80e0e8706a6ed094b8dab2
barpit20 <arpit16229@iiitd.ac.in>
2017-11-15 14:36:24 +0530

    Fix makeMove and explode

--- Code/src/main/Grid96.java
+++ Code/src/main/Grid96.java
@@ -29,13 +29,23 @@ public class Grid96 extends Application {
 	static float cell_small = 50, cell_big = 54;
 	int xc, yc;
 	int turncounter=0;
-	ArrayList<Player> players ;
+	static ArrayList<Player> players ;
+	static Player current;
 	//int currentx;
 	//int currenty;
+	public static void cgc(Rectangle[][] vis, Player p) {
+		for(int i=0;i<rows;i++) {
+			for(int j=0;j<col;j++) {
+				vis[i][j].setStroke(p.color);
+			}
+			}
+	}
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Player current=new Player("Hello",Color.RED);
+		current=new Player("p1",Color.RED);
+		Player other=new Player("p2",Color.BLUE);
 		players = new ArrayList<Player>();
+		players.add(other);
 		Group root = new Group();
 		Scene scene = new Scene(root, 400,550, Color.BLACK);
 		primaryStage.setScene(scene);
@@ -44,6 +54,7 @@ public class Grid96 extends Application {
 		int noOfPlayers = 2;
 		int startx=50;
 		int starty=50;
+		Rectangle[][] vis=new Rectangle[rows][col];
 		for(int i=0;i<rows;i++) {
 			for(int j=0;j<col;j++) {
 				array[i][j] = new Cell(i, j);
@@ -53,14 +64,23 @@ public class Grid96 extends Application {
 				root.getChildren().add(array[i][j].branch);
 				Integer I = new Integer(i);
 				Integer J = new Integer(j);
-				Rectangle a=new Rectangle(50,50,Color.TRANSPARENT);
-				a.setStroke(Color.BLUEVIOLET);
-				root.getChildren().add(a);
-				a.setLayoutX(startx);
-				a.setLayoutY(starty);
-				a.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
+				vis[i][j]=new Rectangle(50,50,Color.TRANSPARENT);
+				vis[i][j].setStroke(current.color);
+				root.getChildren().add(vis[i][j]);
+				vis[i][j].setLayoutX(startx);
+				vis[i][j].setLayoutY(starty);
+				vis[i][j].addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
+					if(array[I][J].owner==null || array[I][J].owner==current) {
+					/*if(turncounter+1>3) {
+						if(players.size()==0) {
+						System.out.println(current.name+" wins");
+						System.exit(0);
+					}
+					}*/
 					turncounter++;
 					makeMove (array[I][J], current, players);
+					cgc(vis,current);
+					}
 				});
 				startx += 50;
 			}
@@ -71,45 +91,10 @@ public class Grid96 extends Application {
 			for(int j=0;j<col;j++) {
 		array[i][j].findNeighbours(array);
 			}}
-		// Remove once settings is integrated
-		/*String name;
-		for (int i = 0; i < noOfPlayers; i++) {
-			//System.out.println("Enter player " + (i + 1) + "'s name and color");
-			name = "hello";
-			Color color = Color.RED;
-			players.add(new Player(name, color));
-		}
-		int turn=0;
-		while (players.size() != 1) {
-			for (int j = 0; j < players.size(); j++) {
-				current=players.get(j);
-				//System.out.println(turncounter);
-				/*while(true) {
-					if(turncounter>turn) {
-					makeMove (array[currentx][currenty], current, players);
-					System.out.println(turncounter);
-					turn++;
-					break;
-					}
-				}*/
-				/*
-				for (int a = 0; a < players.size(); a++) {
-					if (players.get(a).noOfCells == 0) {
-						System.out.println(players.get(a).name + players.get(a).noOfCells);
-						players.remove(a);
-						a = 0;
-					}
-				}
-			}
-		}*/
-
-		// System.out.println(players.get(0).name + " wins!!" );
-		// System.exit(0);
 
 	}
 
 	public static void explode(Player p, Cell c, ArrayList<Player> list) {
-		if(c.owner.equals(p)) {
 			c.owner=null;
 			c.orbNumber=0;
 			c.rot.stop();
@@ -167,88 +152,36 @@ public class Grid96 extends Application {
 					t4.play();
 				}
 			}
-			
-			
-			///////// SEE THIS VVVIMP
-			/*for (int i = 0; i < c.criticalMass; i++) {
-				t.get(i).play();
-			}*/
-		}
-		
-		//System.out.println("vgvg");
-		/*if (c.getOwner() != null) {
-			System.out.println("vgvg");
-			int j = 0;
-			while (j < list.size()) {
-				if (list.get(j).equals(c.getOwner())) {
-					list.get(j).subCell();
-					break;
-				}
-				j++;
-			}
-			c.rot.stop();
-			c.branch.setRotate(0);
-			Material kMaterial = new PhongMaterial();
-			TranslateTransition t1 = new TranslateTransition(Duration.seconds(0.5), c.balls.get(0));
-			TranslateTransition t2 = new TranslateTransition(Duration.seconds(0.5), c.balls.get(1));
-			TranslateTransition t3 = new TranslateTransition(Duration.seconds(0.5), c.balls.get(2));
-			TranslateTransition t4 = new TranslateTransition(Duration.seconds(0.5), c.balls.get(3));
-			ArrayList<TranslateTransition> t = new ArrayList<TranslateTransition>();
-			t.add(t1);
-			t.add(t2);
-			t.add(t3);
-			t.add(t4);
-
-			for (int i = 0; i < c.neighbours.size(); i++) {
-				//System.out.println(c.neighbours.get(0).orbNumber);
-				if (c.neighbours.get(i).x > c.x) {
-					t.get(i).setFromX(c.branch.getLayoutX());
-					t.get(i).setToX(c.branch.getLayoutX() + 50);
-					Integer I=new Integer(i);
-					t.get(i).setOnFinished(new EventHandler<ActionEvent>() {
-						public void handle(ActionEvent event) {
-							makeMove (c.neighbours.get(I), p,list);
-						}
-					});
-				} else if (c.neighbours.get(i).x < c.x) {
-					t.get(i).setFromX(c.branch.getLayoutX());
-					t.get(i).setToX(c.branch.getLayoutX() - 50);
-				} else if (c.neighbours.get(i).y < c.y) {
-					t.get(i).setFromY(c.branch.getLayoutY());
-					t.get(i).setToY(c.branch.getLayoutY() - 50);
-				} else if (c.neighbours.get(i).y > c.y) {
-					t.get(i).setFromY(c.branch.getLayoutY());
-					t.get(i).setToY(c.branch.getLayoutY() + 50);
-				}
-			}
-			((PhongMaterial) kMaterial).setDiffuseColor(p.color);
-			for (int i = 0; i < 4; i++) {
-				c.balls.get(i).setMaterial(kMaterial);
-			}
-			c.branch.getChildren().add(c.balls.get(c.criticalMass - 1));
-			///////// SEE THIS VVVIMP
-			for (int i = 0; i <= c.criticalMass; i++) {
-				t.get(i).play();
-			}
-		}*/
 	}
 	public static void makeMove (Cell c, Player p, ArrayList<Player> list){
+		if(!(c.owner==null || c.owner==p)) {
+			c.switcho(p);
+		}
 		int x=c.x;
 		int y=c.y;
-		//System.out.println(x+" "+y);
 		if(array[x][y].getorbs() < array[x][y].criticalMass-1){
 		if(array[x][y].getorbs() == 0){
 		array[x][y].setOwner(p);
+		//System.out.println(x+" "+y);
 		p.addCell();
 		}
 		array[x][y].addOrb();
-		array[x][y].setOwner(p);
-
+		
 		}
 		else{
 		p.subCell();
 		explode(p,c,list);
 		}
+		players.add(current);
+		System.out.println(players.size());
+		System.out.println(current.name+" has no of cells= "+current.getCells());
+		current=players.get(0);
+		for(int o=0;o<players.size();o++) {
+			if(players.get(o).noOfCells==0) {
+				players.remove(0);
+			}
+		}
+		players.remove(0);
 		}
 	public static void main(String[] args) {
 		launch(args);
